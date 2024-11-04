f2fd8f9476f85710232ea15174578154ee8877ff
Euan <ew154461@renishaw.com>
2024-11-04 15:17:58 +0000

    Completed app.js and index.test.js

--- index.test.js
+++ index.test.js
@@ -3,20 +3,28 @@ const { execSync } = require('child_process');
 execSync('npm install');
 execSync('npm run seed');
 
-const request = require("supertest")
+const request = require("supertest");
 const { db } = require('./db/connection');
-const { Musician } = require('./models/index')
+const { Musician } = require('./models/index');
 const app = require('./src/app');
-const {seedMusician} = require("./seedData");
-
+const { seedMusician } = require("./seedData");
 
 describe('./musicians endpoint', () => {
     // Write your tests here
-    
-    
-
-
-
-
-    
-})
+    test("Testing musicians endpoint", async () => {
+        // Sends request to `/musicians` endpoint
+        const response = await request(app).get("/musicians");
+        
+        // Check if the status code is 200
+        expect(response.statusCode).toBe(200);
+        
+        // Parse the response text to JSON
+        const responseData = JSON.parse(response.text);
+        
+        // Write expect tests here
+        expect(responseData).toBeInstanceOf(Array);
+        expect(responseData.length).toBeGreaterThan(0);
+        expect(responseData[0]).toHaveProperty("name");
+        expect(responseData[0]).toHaveProperty("instrument");
+    });
+});
